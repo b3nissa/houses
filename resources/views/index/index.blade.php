41e98d396005f79398d265204f3a2646bdc8d34d
Benissa Bentine <ben@customwebsite.nl>
2020-07-11 11:54:43 +0200

    added files

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('content')
+    <div class="hero">
+        <div class="container">
+            <h1>Fundament Homes, het beste aanbod in huizen voor jou!</h1>
+            <a href="#" class="button">Bekijk laatste aanbod</a>
+        </div>
+    </div>
+
+    <div class="stock">
+        <div class="container">
+            <h2> Ons aanbod </h2>
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <div class="stock-wrapper">
+                        <img src="https://cloud.funda.nl/valentina_media/129/553/699_720.jpg">
+                        <h3> Meerpaal 250 </h3>
+                        <p> 4 kamers </p>
+                        <p class="price"> &euro; 200.000</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="stock-wrapper">
+                        <img src="https://cloud.funda.nl/valentina_media/129/553/699_720.jpg">
+                        <h3> Meerpaal 250 </h3>
+                        <p> 4 kamers </p>
+                        <p class="price"> &euro; 200.000</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="stock-wrapper">
+                        <img src="https://cloud.funda.nl/valentina_media/129/553/699_720.jpg">
+                        <h3> Meerpaal 250 </h3>
+                        <p> 4 kamers </p>
+                        <p class="price"> &euro; 200.000</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<main>
+    <header>
+        <nav>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <a class="navbar-brand" href="{{ route('index.index') }}"><span class="orange">Fundament</span> Houses</a>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="navigation">
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Inloggen</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </nav>
+
+
+    </header>
+    @yield('content')
+</main>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
